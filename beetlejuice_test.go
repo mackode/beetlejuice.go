5becd446b5a0557eb05001900d2a6c5ff6388fb8
Bartek Makowski <biuro@mackode.pl>
2024-12-23 16:15:23 +0000

    Add simple tests

---
+++ beetlejuice_test.go
@@ -0,0 +1,28 @@
+pacakge beetlejuice
+
+import (
+  "testing"
+)
+
+func TestLookup(t *testing.T) {
+  bj := NewBeetleJuice().WithFilePath("data/bj.yaml")
+
+  name := "foo"
+  p, err := bj.Lookup(name)
+  if err != nil {
+    t.Log("name", name, "not found")
+    t.Fail()
+  }
+
+  if p != "bar" {
+    t.Log("name", name, "p", p, "mismatch")
+    t.Fail()
+  }
+
+  name = "nonexist"
+  p, err = bj.Lookup(name)
+  if err == nil {
+    t.Log("name", name, "found")
+    t.Fail()
+  }
+}
